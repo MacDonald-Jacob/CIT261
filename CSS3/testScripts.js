37d37d527faf343fe4d361e5a8a4ace0f43e958c
Jacob MacDonald Account <mac17041@byui.edu>
2019-05-25 16:05:44 -0600

    added css transitions with javascript

---
+++ CSS3/testScripts.js
@@ -0,0 +1,43 @@
+// CHANGE BACKGROUND COLOR
+function changeBG(el,clr){
+    var elem = document.getElementById(el);
+    //transition             property - duration - timing - delay 
+    elem.style.transition = "background 1.0s linear 0s";
+    elem.style.background = clr;
+}
+
+// FADE IN AND OUT
+function fadeOut(el){
+    var elem = document.getElementById(el);
+    elem.style.transition = "opacity 1.0s linear 0s";
+    elem.style.opacity = 0;
+}
+function fadeIn(el){
+    var elem = document.getElementById(el);
+    elem.style.transition = "opacity 1.0s linear 0s";
+    elem.style.opacity = 1;
+}
+
+//SLIDE OPEN AND CLOSE
+function slideOpen(el){
+    var elem = document.getElementById(el);
+    elem.style.transition = "height 1.0s linear 0s";
+    elem.style.height = "200px";
+}
+function slideShut(el){
+    var elem = document.getElementById(el);
+    elem.style.transition = "height 1.0s linear 0s";
+    elem.style.height = "0px";
+}
+
+//Transition
+function slideIn(el){
+    var elem = document.getElementById(el);
+    elem.style.transition = "left 1.0s ease-in 0s";
+    elem.style.left = "10px";
+}
+function slideOut(el){
+    var elem = document.getElementById(el);
+    elem.style.transition = "left 1.0s ease-out 0s";
+    elem.style.left = "-400px";
+}
